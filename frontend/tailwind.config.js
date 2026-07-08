7263153138097c2572dd66df94f3505805aecc58
Swaroop Sandanshive <swaroopsandanshive@gmail.com>
2026-07-08 18:53:34 +0530

    chore: initial project setup with frontend and backend scaffolding

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",       // root HTML
+    "./src/**/*.{js,ts,jsx,tsx}", // all React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
